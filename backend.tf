47c575c647bba7933c5e099b146710b2335e44de
OmarChouchane <omar.ch52831@gmail.com>
2026-02-28 13:57:51 +0100

    workflow init

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-secure-tf-state-235423434"
+    bucket         = "my-secure-tf-state-2354234341"
     key            = "terraform/state"
     region         = "eu-west-1"
     encrypt        = true
